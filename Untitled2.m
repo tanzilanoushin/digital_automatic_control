3c50e21404fc290506ee9cc32bb0b75dca526ace
tanzilanoushin <96462278+tanzilanoushin@users.noreply.github.com>
2021-12-21 00:42:02 -0600

    Add files via upload

--- Untitled2.m
+++ Untitled2.m
@@ -1,12 +1,48 @@
-clear all;
+clearvars;
 close all;
 clc;
 
-s = tf('s');
+x0= [randn(1,1); randn(1,1); randn(1,1); randn(1,1); randn(1,1); randn(1,1); randn(1,1);randn(1,1);randn(1,1)];
+tspan= [0 200];
+[t,x]=ode45('project2_1',tspan,x0);
 
-NUM = 0.01;
-DEN = ((0.005*s^2)+(0.06*s)+0.1001);
-sys = NUM/DEN
-[C_pi,info] = pidtune(sys, 'PID')
-s = stepinfo(sys)
+plot(t,x(:,1),'r');
+hold on
+plot(t,x(:,4),'g');
+hold on
+plot(t,x(:,7),'k');
+figure()
+plot(x(:,1),x(:,4))
+A=hilbert(x(:,1));
+figure()
+plot(A)
+title('hilbert of xi')
+figure()
+plot(abs(A))
+title('Xi')
+hold on 
+plot(unwrap(angle(A)))
+
+A_=hilbert(x(:,4));
+hold on
+plot(abs(A_),'g')
+
+hold on 
+plot(unwrap(angle(A_)),'k')
+figure()
+plot(A_)
+title('hilbert of xj')
+cor=xcorr(x(:,1),x(:,4));
+figure()
+plot(cor)
+[c,lags] = xcorr(x(:,1),x(:,4),500,'normalized');
+figure()
+stem(lags,c)
+[r,p]=corrcoef(x(:,1),x(:,4));
+figure()
+plot(r)
+hold on
+plot(p)
+figure()
+plot(mscohere(x(:,1),x(:,4)))
 
--- project2_1.m
+++ project2_1.m
@@ -1,20 +1,16 @@
+function xdot=project2_1(t,x)
 
-function xdot=project2_1(t,x,N)
+b=3.0; r=0.006; xo=-1.67; I=2.8;k =2;  
 
-b=3.0; r=0.006; xo=-1.67; I=2.8;k =2; 
-A = zeros(1,N);
-
- for x = 0:1:N
-     for y = 1:1:N
-         A(1,N)=x(1+3*N);
-     end
-     xdot(N+1)= x(N+2)-x(N+1).^3+b*x(N+1).^2-x(N+3)+I ;
-     xdot(N+2)=1-5*x(N+1).^2-x(N+2);
-     xdot(N+3)= r*(4*(x(N+1)-xo)-x(N+3));
- end
- 
-
-
-%xdot=xdot';
+xdot(1)= x(2)-x(1).^3+b*x(1).^2-x(3)+I+k.*mean([x(4), x(1),x(7)]) ;
+xdot(2)=1-5*x(1).^2-x(2);
+xdot(3)= r*(4*(x(1)-xo)-x(3));
+xdot(4) = x(5)-x(4).^3+b*x(4).^2-x(6)+I+k.*mean([ x(1),  x(4), x(7)]);
+xdot(5)= 1-5*x(4).^2-x(5);
+xdot(6)= r*(4*(x(4)-xo)-x(6));
+xdot(7) = x(8)-x(7).^3+b*x(7).^2-x(9)+I+k.*mean([ x(1),  x(4), x(7)]);
+xdot(8)= 1-5*x(7).^2-x(8);
+xdot(9)= r*(4*(x(7)-xo)-x(9));
+xdot=xdot';
 
 end
