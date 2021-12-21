827c6de560720d3352c161f12fccc6f5920f83b1
tanzilanoushin <96462278+tanzilanoushin@users.noreply.github.com>
2021-12-21 00:27:59 -0600

    Add files via upload

--- Untitled2.m
+++ Untitled2.m
@@ -1,26 +1,12 @@
-clc;
 clear all;
 close all;
-x=[0.00001;20;40;60;80;100;120;140;180;200;220];
-y=[11.32333;11.33667;11.35;11.35667;11.38667;11.41667;11.45667;11.46;11.50667;11.56667;11.60333];
-%w =[1;1;1;1;1;1];
-w =[1;1;1;1;1;1;1;1;1;3;6];
-plot(x,y,'.');
-ft = fittype('power2');
-[cf,g] = fit(x,y,ft,'Weight',w);
-hold on
-plot(cf,'fit');
-cf 
-g
-%[f,g] = fit(x,y,'power2')
-%plot(f,x,y)
-hold on
-sd_vct = [0.008819 0.006667 0.005774 0.013333 0.0120019 0.017638 0.016667 0.015275 0.012019 0.008819 0.01219];
-e = errorbar(x,y,sd_vct,'.');
-e.Marker = '.';
-e.MarkerSize = 10;
-e.Color = 'black';
-e.CapSize = 4;
-xlabel('Strain%')
-ylabel('Resistance(k ohm)')
+clc;
+
+s = tf('s');
+
+NUM = 0.01;
+DEN = ((0.005*s^2)+(0.06*s)+0.1001);
+sys = NUM/DEN
+[C_pi,info] = pidtune(sys, 'PID')
+s = stepinfo(sys)
 
